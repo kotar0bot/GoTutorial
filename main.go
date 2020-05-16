d4d7d4b289bdec7ddf212a3fa7bd1f01663d2a1a
crazyma-nn <iwn001bim0@gmail.com>
2020-05-16 22:39:25 +0900

    重複箇所をメソッドに切り出し

--- main.go
+++ main.go
@@ -88,30 +88,21 @@ type Todo struct {
 
 //DB初期化
 func dbInit() {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！（dbInit）")
-	}
+	db := dbOpen()
 	db.AutoMigrate(&Todo{})
 	defer db.Close()
 }
 
 //DB追加
 func dbInsert(text string, status string) {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！（dbInsert)")
-	}
+	db := dbOpen()
 	db.Create(&Todo{Text: text, Status: status})
 	defer db.Close()
 }
 
 //DB更新
 func dbUpdate(id int, text string, status string) {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！（dbUpdate)")
-	}
+	db := dbOpen()
 	var todo Todo
 	db.First(&todo, id)
 	todo.Text = text
@@ -122,10 +113,7 @@ func dbUpdate(id int, text string, status string) {
 
 //DB削除
 func dbDelete(id int) {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！（dbDelete)")
-	}
+	db := dbOpen()
 	var todo Todo
 	db.First(&todo, id)
 	db.Delete(&todo)
@@ -134,10 +122,7 @@ func dbDelete(id int) {
 
 //DB全取得
 func dbGetAll() []Todo {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！(dbGetAll())")
-	}
+	db := dbOpen()
 	var todos []Todo
 	db.Order("created_at desc").Find(&todos)
 	db.Close()
@@ -146,12 +131,17 @@ func dbGetAll() []Todo {
 
 //DB一つ取得
 func dbGetOne(id int) Todo {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！(dbGetOne())")
-	}
+	db := dbOpen()
 	var todo Todo
 	db.First(&todo, id)
 	db.Close()
 	return todo
+}
+
+func dbOpen() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("データベース開けず！(dbOpen)")
+	}
+	return db
 }
